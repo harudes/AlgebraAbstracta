e76dd0079e4d4ee345b556ddf645c1c38017a33b
harudes <luis.rendon@ucsp.edu.pe>
2017-04-27 11:16:59 -0500

    RSA completp

--- RSA/main.cpp
+++ RSA/main.cpp
@@ -7,7 +7,36 @@
 using namespace std;
 using namespace NTL;
 
+class bbs{
 
+       ZZ p, q, M, seed, actual;
+       public:
+       bbs(ZZ p, ZZ q, ZZ s){
+            this->p = p;
+            this->q = q;
+            this->seed = s;
+            M = p * q;
+            actual = s;
+       }
+       ZZ getrandom(){
+            ZZ r = to_ZZ(fmod(to_int(actual*actual),to_int(M)));
+            actual = r;
+            return r;
+       }
+       ZZ getirandom(int i){
+            ZZ g = modulo(p, q);
+            ZZ lcm = p * q / g;
+            ZZ exp = to_ZZ(1);
+            for(int j = 1; j <= i; ++j){
+                exp = to_ZZ(fmod(to_long(exp+exp),to_long(lcm)));
+            }
+            ZZ x0 = seed * seed;
+            ZZ r = x0;
+            for(int j = 2; j <= to_int(exp); ++j){
+                r = to_ZZ(fmod(to_long(r*x0),to_long(M)));
+        }
+        }
+   };
 
 class Cesar{
     ZZ clave;
